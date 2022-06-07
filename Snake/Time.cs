81a684e8543b5602e8297a91eda131fbff172a9c
Fil31 <lolid76@gmail.com>
2022-06-07 16:26:15 +0300

    first comm

---
+++ Snake/Time.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Time
+    {
+   
+            static void Main(string[] args)
+            {
+             
+
+                DateTime dateTime = new DateTime();
+                Console.WriteLine(dateTime);
+                Console.ReadKey();
+
+            }
+     
+    }
+}
+}
---
+++ Snake/score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class score
+    {
+        static void InitScore()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(1, 0);
+            Console.Write("Score: 0");
+        }
+    }
+}
